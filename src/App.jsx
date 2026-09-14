0183da5834729c74d55c7d533ad01dfb2016138c
Doaa-Esam <doaaelshenawy39@gmail.com>
2026-09-15 02:47:17 +0300

    Featured Product

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,23 @@ import { OrderProvider } from "./contexts/OrderContext.jsx";
 import { UserProvider } from "./contexts/UserContext";
 import { WishlistProvider } from "./contexts/WishlistContext.jsx";
 import { CartProvider } from "./contexts/CartContext.jsx";
+import FeaturedProducts from "./components/FeaturedProducts.jsx";
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <UserProvider>
-          <ProductProvider>
-            <OrderProvider>
-              <WishlistProvider>
-                <CartProvider>
-                  <div>App</div>
-                </CartProvider>
-              </WishlistProvider>
-            </OrderProvider>
-          </ProductProvider>
-        </UserProvider>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <UserProvider>
+        <ProductProvider>
+          <OrderProvider>
+            <WishlistProvider>
+              <CartProvider>
+                <FeaturedProducts />
+              </CartProvider>
+            </WishlistProvider>
+          </OrderProvider>
+        </ProductProvider>
+      </UserProvider>
+    </BrowserRouter>
   );
 };
 
