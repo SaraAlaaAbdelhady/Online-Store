34033eaf18c280b7ef003dea5eac7fa6dd21bf14
Sara Abdelhady <saraalaa09@outlook.com>
2026-09-16 17:52:11 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ProductProvider } from "./contexts/ProductContext.jsx";
 import { OrderProvider } from "./contexts/OrderContext.jsx";
 import { UserProvider } from "./contexts/UserContext";
 import { WishlistProvider } from "./contexts/WishlistContext.jsx";
 import { CartProvider } from "./contexts/CartContext.jsx";
+import Home from "./pages/Home.jsx";
+import Footer from "./components/home/Footer.jsx";
+// import ShopByCategory from "./components/category.jsx";
 
 const App = () => {
   return (
@@ -15,7 +17,11 @@ const App = () => {
             <OrderProvider>
               <WishlistProvider>
                 <CartProvider>
-                  <div>App</div>
+                  <Routes>
+                  <Route path="/" element={<Home />} />
+        
+                </Routes>
+                <Footer/>
                 </CartProvider>
               </WishlistProvider>
             </OrderProvider>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import HowItWorks from  "../components/home/HowItWorks"
+
+import StayUpdated from  "../components/home/StayUpdated"
+
+
+const Home = () => {
+
+
+
+  return (
+    <>
+    <HowItWorks />
+    <StayUpdated/>
+    
+    </>
+    
+  )
+}
+
+export default Home
